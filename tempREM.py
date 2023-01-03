860e009a97a076dfc9c6ec4481ef429c2278b3a4
thepatriotscommandxs <thepatriotsfanxs@gmail.com>
2023-01-02 19:49:23 -0600

    dart 2 parse

--- tempREM.py
+++ tempREM.py
@@ -1,4 +1,5 @@
-import os,sys
+import os,sys,re,PyPDF2
+
 fileName = sys.argv[0].split("/")[::-1][0].split(".")[1]
 
 if (fileName == "py"):
@@ -12,37 +13,57 @@ items = os.listdir(__location__+"\\data")
 
 hint = []
 power = []
+answer = []
+fulltext=''
 
-with open(__location__+"\\rams.txt", "r",encoding="utf-8") as f:
-    fulltext = f.readlines()
+# Open the PDF file in binary mode
+with open(__location__+"\\data\\"+items[0], 'rb') as file:
+    # Create a PDF object
+    pdf = PyPDF2.PdfFileReader(file)
+
+    # Get the number of pages in the PDF
+    num_pages = pdf.getNumPages()
+
+    # Iterate over each page
+    for page_num in range(num_pages):
+        # Get the page object
+        page = pdf.getPage(page_num)
+
+        # Extract the text from the page
+        text = page.extractText()
+
+        # Print the text
+
+
+        fulltext += text
+        
+fulltext = fulltext.replace("\n","")
 print(fulltext)
-
 for i in range(len(fulltext)):
-    hint.append(fulltext[i].split(". ",1)[1])
+    try:
+        fulltext[i] = fulltext[i].split(". ",1)[1]
+        fulltext[i] = re.sub("\[(?:[^]])*\]","",fulltext[i])
+        fulltext[i] = fulltext[i].replace("\n","")
 
+        power.append(fulltext[i].split(". ",1)[0])
+        hint.append(fulltext[i].split(". ",1)[1])
+    except:
+        pass
 
-print(hint)
+for i in range(len(hint)):
+    answer.append(hint[i].split("ANSWER: ")[1])
 
+for i in range(len(hint)):
+    hint[i] = hint[i].split("ANSWER: ")[0]
+    
 
-#for i in range(len(hint)):
-#    hint[i] = re.sub("AN+[a-zA-Z0-9 ]{4,5}: ","",hint[i])
-#    hint[i] = hint[i].replace("(*)","")
-#    hint[i] = re.sub("\[(?:[^]])*\]","",hint[i])
-#    power.append(hint[i].split(". ")[0])
-#    split = hint[i].split(". ")[0]
-#    hint[i] = hint[i].replace(split,"")
-#
-#for i in range(len(answer)):
-#    answer[i] = answer[i].replace("ANSWER: ","")
-#    answer[i] = answer[i].replace("<","")
-#    answer[i] = re.sub("\[.*\]","",answer[i])
-#    answer[i] = re.sub("\(.*\)","",answer[i])
-#    answer[i] = re.sub("\((?:[^)])*\)","",answer[i])
-#
-#for i in range(len(answer)):
-#    try:
-#        with open(__location__+"\\set"+str(c)+".txt","a", encoding="utf-8") as f:
-#            f.write("['"+answer[i]+"', '"+power[i]+"||"+hint[i]+"']"+"\n")
-#            f.close()
-#    except:
-#        break
+for i in range(len(answer)):
+    answer[i] = re.sub("\((?:[^)])*\)","",answer[i])
+
+for i in range(len(answer)):
+    try:
+        with open(__location__+"\\set.txt","a", encoding="utf-8") as f:
+            f.write("['"+answer[i]+"', '"+power[i]+" || "+hint[i]+"']"+"\n")
+            f.close()
+    except:
+        break
